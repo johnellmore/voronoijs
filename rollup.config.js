091b2c02e7a383efeba869e92299d5e476b3b739
John Ellmore <jnellmore@gmail.com>
2020-01-20 18:01:16 -0600

    Reworked rollup/jest config to allow ES modules

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,38 @@
-export default {
-  input: 'src/main.js',
-  output: {
-    file: 'bundle.js',
-    format: 'cjs'
+import resolve from "rollup-plugin-node-resolve";
+import commonjs from "rollup-plugin-commonjs";
+import pkg from "./package.json";
+import babel from "rollup-plugin-babel";
+
+export default [
+  // browser-friendly UMD build
+  {
+    input: "src/Voronoi.js",
+    output: {
+      name: "voronoi",
+      file: pkg.browser,
+      format: "umd"
+    },
+    plugins: [
+      resolve(),
+      commonjs(),
+      babel({
+        exclude: "node_modules/**"
+      })
+    ]
+  },
+
+  // CommonJS (for Node) and ES module (for bundlers) build.
+  // (We could have three entries in the configuration array
+  // instead of two, but it's quicker to generate multiple
+  // builds from a single configuration where possible, using
+  // an array for the `output` option, where we can specify
+  // `file` and `format` for each target)
+  {
+    input: "src/Voronoi.js",
+    external: [],
+    output: [
+      { file: pkg.main, format: "cjs" },
+      { file: pkg.module, format: "es" }
+    ]
   }
-};
+];
