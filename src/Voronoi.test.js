7137f83cef7274dfff1c39ae9d031f24dca761e1
John Ellmore <jnellmore@gmail.com>
2020-01-26 15:42:47 -0600

    Added larger test (50 sites, randomly picked)

--- src/Voronoi.test.js
+++ src/Voronoi.test.js
@@ -1,3 +1,4 @@
+import seedrandom from "seedrandom";
 import Voronoi from "./Voronoi";
 
 test("Can make a diagram from an empty set of sites", () => {
@@ -7,7 +8,7 @@ test("Can make a diagram from an empty set of sites", () => {
   expect(diagram.prototype).toBe(Voronoi.Diagram);
 });
 
-test("Can make a diagram from two sites", () => {
+test("Makes a correct diagram from two sites", () => {
   const v = new Voronoi();
   const boundingBox = { xl: 0, xr: 100, yt: 0, yb: 100 };
   const sites = [
@@ -22,4 +23,29 @@ test("Can make a diagram from two sites", () => {
   const dividingEdge = dividingEdges[0];
   expect(dividingEdge.va).toEqual({ x: 0, y: 100 });
   expect(dividingEdge.vb).toEqual({ x: 100, y: 0 });
+});
+
+test("Can make a diagram from fifty sites", () => {
+  // generate random points
+  const width = 600;
+  const height = 400;
+  const margin = 20;
+  const boundingBox = { xl: 0, xr: width, yt: 0, yb: height };
+  const effectiveWidth = width - margin * 2;
+  const effectiveHeight = height - margin * 2;
+
+  const rand = seedrandom("fity-sites-test");
+  const sites = [];
+  for (let i = 0; i < 50; i += 1) {
+    sites.push({
+      x: Math.round((margin + rand() * effectiveWidth) * 10) / 10,
+      y: Math.round((margin + rand() * effectiveHeight) * 10) / 10
+    });
+  }
+
+  const v = new Voronoi();
+  const diagram = v.compute(sites, boundingBox);
+
+  expect(diagram.prototype).toBe(Voronoi.Diagram);
+  expect(diagram.cells).toHaveLength(50);
 });
